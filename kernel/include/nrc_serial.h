aaed7052b096d6ec6eb41fea2233927a25697d96
Tomas Frisberg <tomasfrisberg@gmail.com>
2017-12-29 09:45:26 +0100

    Started defining uart/serial components.

---
+++ kernel/include/nrc_serial.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2017 Tomas Frisberg & Ola Bjorsne
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http ://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _NRC_SERIAL_H_
+#define _NRC_SERIAL_H_
+
+#include "nrc_types.h"
+#include "nrc_port.h"
+#include "nrc_node.h"
+
+typedef void* nrc_serial_t;
+
+struct nrc_serial_usr {
+    u32_t                   evt;
+    nrc_node_t              node;
+};
+
+s32_t nrc_serial_init(void);
+
+s32_t nrc_serial_open(
+    s8_t                    *port_name,
+    void                    *port_settings,
+    struct nrc_serial_usr   *data_available,
+    struct nrc_serial_usr   *write_complete,
+    nrc_serial_t            *port);
+
+u32_t nrc_serial_read(nrc_serial_t port, u8_t *buf, u32_t buf_size);
+
+u32_t nrc_serial_write(nrc_serial_t port, u8_t *buf, u32_t buf_size);
+
+#endif
---
+++ port/include/nrc_port_uart.h
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2017 Tomas Frisberg & Ola Bjorsne
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http ://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _NRC_PORT_UART_H_
+#define _NRC_PORT_UART_H_
+
+#include "nrc_types.h"
+#include "nrc_port.h"
+
+typedef void* nrc_port_uart_t;
+
+enum nrc_port_serial_parity {
+    NRC_PORT_UART_PARITY_NONE,
+    NRC_PORT_UART_PARITY_ODD,
+    NRC_PORT_UART_PARITY_EVEN
+};
+
+enum nrc_port_uart_flow_ctrl {
+    NRC_PORT_UART_FLOW_NONE,
+    NRC_PORT_UART_FLOW_HW
+};
+
+struct nrc_port_uart_pars {
+    u8_t                            port;
+    u32_t                           baud_rate;
+    u8_t                            data_bits;
+    enum nrc_port_serial_parity     parity;
+    u8_t                            stop_bits;
+    enum nrc_port_uart_flow_ctrl    flow_ctrl;
+};
+
+typedef void(*nrc_port_uart_data_available_t)(nrc_port_uart_t uart);
+typedef void(*nrc_port_uart_write_complete_t)(nrc_port_uart_t uart, s32_t result);
+typedef void(*nrc_port_uart_error_t)(nrc_port_uart_t uart, s32_t error);
+
+struct nrc_port_uart_callback_fcn {
+    nrc_port_uart_data_available_t    data_available;
+    nrc_port_uart_write_complete_t    write_complete;
+    nrc_port_uart_error_t             error;
+};
+
+s32_t nrc_port_uart_init(void);
+
+s32_t nrc_port_uart_open(
+    struct nrc_port_uart_pars           pars,
+    struct nrc_port_uart_callback_fcn   callback,
+    nrc_port_uart_t                     *uart);
+
+s32_t nrc_port_uart_close(nrc_port_uart_t uart);
+
+s32_t nrc_port_uart_write(nrc_port_uart_t uart, u8_t *buf, u32_t buf_size);
+
+u32_t nrc_port_uart_read(nrc_port_uart_t uart, u8_t *buf, u32_t buf_size);
+
+#endif
