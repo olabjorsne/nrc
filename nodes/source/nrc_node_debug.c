79a366472838ba1d0056a269df7fb44a55d10210
Tomas Frisberg <tomasfrisberg@gmail.com>
2018-02-08 20:44:46 +0100

    Updated inject and debug node according to serial-in structure.

--- nodes/source/nrc_node_debug.c
+++ nodes/source/nrc_node_debug.c
@@ -4,18 +4,23 @@
 #include "nrc_os.h"
 #include "nrc_log.h"
 #include "nrc_factory.h"
+#include "nrc_assert.h"
 #include <string.h>
+
+#define NRC_N_DEBUG_TYPE    (0x927365A1)
 
 enum nrc_node_debug_state {
     NRC_N_DEBUG_S_INVALID = 0,
     NRC_N_DEBUG_S_CREATED,
     NRC_N_DEBUG_S_INITIALISED,
-    NRC_N_DEBUG_S_STARTED
+    NRC_N_DEBUG_S_STARTED,
+    NRC_N_DEBUG_S_ERROR
 };
 
 struct nrc_node_debug {
     struct nrc_node_hdr         hdr;
     enum nrc_node_debug_state   state;
+    u32_t                       type;
 };
 
 static nrc_node_t nrc_node_debug_create(struct nrc_node_factory_pars *pars);
@@ -26,16 +31,24 @@ static s32_t nrc_node_debug_stop(nrc_node_t self);
 static s32_t nrc_node_debug_recv_msg(nrc_node_t self, nrc_msg_t msg);
 static s32_t nrc_node_debug_recv_evt(nrc_node_t self, u32_t event_mask);
 
-static s32_t log_msg(const s8_t *name, nrc_msg_t msg);
+static s32_t log_msg(struct nrc_node_debug *self, nrc_msg_t msg);
 
-const static s8_t* _tag = "debug";
-static struct nrc_node_api _api;
+const static s8_t*          _tag = "debug";
+static struct nrc_node_api  _api;
+
+void nrc_node_debug_register(void)
+{
+    s32_t status = nrc_factory_register_node_type("nrc-debug", nrc_node_debug_create);
+    if (!OK(status)) {
+        NRC_LOGE(_tag, "register: error %d", status);
+    }
+}
 
 static nrc_node_t nrc_node_debug_create(struct nrc_node_factory_pars *pars)
 {
     struct nrc_node_debug *node = NULL;
 
-    if ((pars != NULL) && (strcmp("debug", pars->cfg_type) == 0)) {
+    if ((pars != NULL) && (strcmp("nrc-debug", pars->cfg_type) == 0)) {
         node = (struct nrc_node_debug*)nrc_os_node_alloc(sizeof(struct nrc_node_debug));
 
         if (node != NULL) {
@@ -54,19 +67,18 @@ static nrc_node_t nrc_node_debug_create(struct nrc_node_factory_pars *pars)
 
             pars->api = &_api;
 
+            node->type = NRC_N_DEBUG_TYPE;
             node->state = NRC_N_DEBUG_S_CREATED;
         }
+        else {
+            NRC_LOGE(_tag, "create(%s): out of mem", pars->cfg_id);
+        }
+    }
+    else {
+        NRC_LOGE(_tag, "create: invalid in params");
     }
 
     return node;
-}
-
-void nrc_node_debug_register(void)
-{
-    s32_t status = nrc_factory_register_node_type("debug", nrc_node_debug_create);
-    if (!OK(status)) {
-        NRC_LOGE(_tag, "Registration to factory failed")
-    }
 }
 
 static s32_t nrc_node_debug_init(nrc_node_t slf)
@@ -74,30 +86,56 @@ static s32_t nrc_node_debug_init(nrc_node_t slf)
     struct nrc_node_debug  *self = (struct nrc_node_debug*)slf;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
-    if (self != 0) {
+    if ((self != NULL) && (self->type == NRC_N_DEBUG_TYPE)){
         switch (self->state) {
         case NRC_N_DEBUG_S_CREATED:
-            //TODO:
+            // Nothing to init
 
             self->state = NRC_N_DEBUG_S_INITIALISED;
             result = NRC_R_OK;
 
-            NRC_LOGD("debug", "initialised");
+            NRC_LOGI(_tag, "init(%s): ok", self->hdr.cfg_id);
             break;
 
         default:
-            NRC_LOGD("debug", "init: invalid state %d", self->state);
+            NRC_LOGE(_tag, "init(%s): invalid state %d", self->hdr.cfg_id, self->state);
             result = NRC_R_INVALID_STATE;
             break;
         }
+    }
+    else {
+        NRC_LOGE(_tag, "init: invalid in params");
     }
 
     return result;
 }
 
-static s32_t nrc_node_debug_deinit(nrc_node_t self)
+static s32_t nrc_node_debug_deinit(nrc_node_t slf)
 {
-    s32_t result = NRC_R_OK;
+    struct nrc_node_debug  *self = (struct nrc_node_debug*)slf;
+    s32_t                   result = NRC_R_INVALID_IN_PARAM;
+
+    if ((self != NULL) && (self->type == NRC_N_DEBUG_TYPE)) {
+        switch (self->state) {
+        case NRC_N_DEBUG_S_INITIALISED:
+        case NRC_N_DEBUG_S_ERROR:
+            // Nothing to free
+
+            self->state = NRC_N_DEBUG_S_CREATED;
+            result = NRC_R_OK;
+
+            NRC_LOGI(_tag, "deinit(%s): ok", self->hdr.cfg_id);
+            break;
+
+        default:
+            NRC_LOGE(_tag, "deinit(%s): invalid state %d", self->hdr.cfg_id, self->state);
+            result = NRC_R_INVALID_STATE;
+            break;
+        }
+    }
+    else {
+        NRC_LOGE(_tag, "deinit: invalid in params");
+    }
 
     return result;
 }
@@ -107,21 +145,24 @@ static s32_t nrc_node_debug_start(nrc_node_t slf)
     struct nrc_node_debug  *self = (struct nrc_node_debug*)slf;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
-    if (self != 0) {
+    if ((self != NULL) && (self->type == NRC_N_DEBUG_TYPE)) {
         switch (self->state) {
         case NRC_N_DEBUG_S_INITIALISED:
 
             self->state = NRC_N_DEBUG_S_STARTED;
             result = NRC_R_OK;
 
-            NRC_LOGD("debug", "started");
+            NRC_LOGI(_tag, "start(%s): ok", self->hdr.cfg_id);
             break;
 
         default:
-            NRC_LOGD("debug", "start: invalid state %d", self->state);
+            NRC_LOGE(_tag, "start(%s): invalid state %d", self->hdr.cfg_id, self->state);
             result = NRC_R_INVALID_STATE;
             break;
         }
+    }
+    else {
+        NRC_LOGE(_tag, "start: invalid in params");
     }
 
     return result;
@@ -132,18 +173,23 @@ static s32_t nrc_node_debug_stop(nrc_node_t slf)
     struct nrc_node_debug  *self = (struct nrc_node_debug*)slf;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
-    if (self != 0) {    
+    if ((self != NULL) && (self->type == NRC_N_DEBUG_TYPE)) {
         switch (self->state) {
         case NRC_N_DEBUG_S_STARTED:
             self->state = NRC_N_DEBUG_S_INITIALISED;
             result = NRC_R_OK;
+
+            NRC_LOGI(_tag, "stop(%s): ok", self->hdr.cfg_id);
             break;
 
         default:
-            NRC_LOGD("debug", "stop: invalid state %d", self->state);
+            NRC_LOGE(_tag, "stop(%s): invalid state %d", self->hdr.cfg_id, self->state);
             result = NRC_R_INVALID_STATE;
             break;
         }
+    }
+    else {
+        NRC_LOGE(_tag, "stop: invalid in params");
     }
 
     return result;
@@ -154,17 +200,23 @@ static s32_t nrc_node_debug_recv_msg(nrc_node_t slf, nrc_msg_t msg)
     struct nrc_node_debug   *self = (struct nrc_node_debug*)slf;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
-    if ((self != NULL) && (msg != NULL)) {
+    if ((self != NULL) && (self->type == NRC_N_DEBUG_TYPE) && (msg != NULL)) {
 
         switch (self->state) {
         case NRC_N_DEBUG_S_STARTED:
-            result = log_msg(self->hdr.cfg_name, msg);
+            result = log_msg(self, msg);
+
+            NRC_LOGV(_tag, "recv_msg(%s): result %d", self->hdr.cfg_id, result);
             break;
+
         default:
-            NRC_LOGD("debug", "recv_msg: invalid state %d", self->state);
+            NRC_LOGE(_tag, "recv_msg(%s): invalid state %d", self->hdr.cfg_id, self->state);
             result = NRC_R_INVALID_STATE;
             break;
         }
+    }
+    else {
+        NRC_LOGE(_tag, "recv_msg: invalid in params");
     }
 
     if (msg != NULL) {
@@ -179,54 +231,65 @@ static s32_t nrc_node_debug_recv_evt(nrc_node_t slf, u32_t event_mask)
     struct nrc_node_debug  *self = (struct nrc_node_debug*)slf;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
-    if (self != NULL) {
+    if ((self != NULL) && (self->type == NRC_N_DEBUG_TYPE)) {
         switch (self->state) {
         case NRC_N_DEBUG_S_STARTED:
-            NRC_LOGD("debug", "recv_evt: unexpected event %d", event_mask);
-            result = NRC_R_ERROR;
+            NRC_LOGW(_tag, "recv_evt(%s): unexpected evt %d", self->hdr.cfg_id, event_mask);
+            result = NRC_R_OK;
             break;
 
         default:
-            NRC_LOGD("nrc_timer", "recv_evt: invalid state %d", self->state);
+            NRC_LOGE(_tag, "recv_evt(%s): invalid state %d", self->hdr.cfg_id, self->state);
             result = NRC_R_INVALID_STATE;
             break;
         }
+    }
+    else {
+        NRC_LOGE(_tag, "recv_evt: invalid in params");
     }
 
     return result;
 }
 
-static s32_t log_msg(const s8_t *name, nrc_msg_t msg)
+static s32_t log_msg(struct nrc_node_debug *self, nrc_msg_t msg)
 {
     s32_t               result = result = NRC_R_INVALID_IN_PARAM;
     struct nrc_msg_hdr  *hdr = (struct nrc_msg_hdr*)msg;
+
+    NRC_ASSERT(self != NULL);
+    NRC_ASSERT(self->type == NRC_N_DEBUG_TYPE);
+
+    // TODO: Messages shall be sent to host
 
     if (hdr != NULL) {
         switch (hdr->type) {
         case NRC_MSG_TYPE_STRING:
         {
             struct nrc_msg_str *msg_str = (struct nrc_msg_str*)hdr;
-            NRC_LOGI(name, "topic: %s, string: %s", hdr->topic, msg_str->str);
+            NRC_LOGI(_tag, "log_msg(%s): topic: %s, string: %s", self->hdr.cfg_id, hdr->topic, msg_str->str);
             break;
         }
         case NRC_MSG_TYPE_INT:
         {
             struct nrc_msg_int *msg_int = (struct nrc_msg_int*)hdr;
-            NRC_LOGI(name, "topic: %s, integer: %d", hdr->topic, msg_int->value);
+            NRC_LOGI(_tag, "log_msg(%s): topic: %s, int: %d", self->hdr.cfg_id, hdr->topic, msg_int->value);
             break;
         }
         case NRC_MSG_TYPE_DATA_AVAILABLE:
         {
             struct nrc_msg_data_available *msg_da = (struct nrc_msg_data_available*)hdr;
-            NRC_LOGI(name, "topic: %s, read fcn: 0x%X", hdr->topic, msg_da->read);
+            NRC_LOGI(_tag, "log_msg(%s): topic: %s, node 0x%X, read fcn: 0x%X", self->hdr.cfg_id, hdr->topic, msg_da->node, msg_da->read);
             break;
         }
         default:
         {
-            NRC_LOGI(name, "topic: %s", hdr->topic);
+            NRC_LOGI(_tag, "log_msg(%s): topic: %s", self->hdr.cfg_id, hdr->topic);
             break;
         }
         }
+    }
+    else {
+        NRC_LOGW(_tag, "log_msg(%s): empty msg");
     }
 
     return result;
