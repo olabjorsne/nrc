dca56c17b7bbcbaadc7778e43d90c5108d7e7a25
Tomas Frisberg <tomasfrisberg@gmail.com>
2018-05-07 19:24:28 +0200

    Coded most of it. Not yet tested.
    Next step is to add the html and js components for the nrc_host node.

--- nodes/source/nrc_node_host.c
+++ nodes/source/nrc_node_host.c
@@ -16,14 +16,24 @@
 
 #include "nrc_types.h"
 #include "nrc_node.h"
+#include "nrc_msg.h"
 #include "nrc_cfg.h"
 #include "nrc_os.h"
 #include "nrc_log.h"
 #include "nrc_factory.h"
 #include <string.h>
 #include "nrc_assert.h"
+#include "nrc_status.h"
+#include "jsmn.h"
 
-#define NRC_N_HOST_TYPE           (0x56483887)
+#define NRC_N_HOST_TYPE             (0x56483887)
+#define NRC_N_HOST_MAX_TOKENS       (100)
+
+enum nrc_node_host_cmd {
+    NRC_N_HOST_CMD_INVALID,
+    NRC_N_HOST_CMD_START,
+    NRC_N_HOST_CMD_STOP
+};
 
 enum nrc_node_host_state {
     NRC_N_HOST_S_INVALID,
@@ -39,6 +49,12 @@ struct nrc_node_host {
 
     enum nrc_node_host_state    state;
     s8_t                        prio;
+
+    nrc_cfg_t                   *cfg;
+
+    jsmn_parser                 json_parser;
+    jsmntok_t                   json_tokens[NRC_N_HOST_MAX_TOKENS];
+    s32_t                       num_tokens;
 
     u32_t                       type;
 };
@@ -102,13 +118,21 @@ nrc_node_t nrc_node_host_create(struct nrc_node_factory_pars *pars)
 
 static s32_t nrc_node_host_init(nrc_node_t slf)
 {
-    struct nrc_node_host  *self = (struct nrc_node_host*)slf;
+    struct nrc_node_host    *self = (struct nrc_node_host*)slf;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
     if ((_host == NULL) && (self != NULL) && (self->type == NRC_N_HOST_TYPE)) {
         switch (self->state) {
         case NRC_N_HOST_S_CREATED:
-            result = nrc_cfg_get_str(self->hdr.cfg_id, "topic", &self->topic);
+            result = nrc_status_init();
+
+            if (OK(result)) {
+                result = nrc_cfg_init();
+            }
+
+            if (OK(result)) {
+                result = nrc_cfg_get_str(self->hdr.cfg_id, "topic", &self->topic);
+            }
             
             if (OK(result)) {
                 // Get node priority
@@ -125,7 +149,9 @@ static s32_t nrc_node_host_init(nrc_node_t slf)
             }
 
             if (OK(result)) {
-                _host = self;
+                jsmn_init(&self->json_parser);
+
+                _host = self; // Singleton
 
                 self->state = NRC_N_HOST_S_INITIALISED;
             }
@@ -179,11 +205,14 @@ static s32_t nrc_node_host_start(nrc_node_t slf)
     if ((self != NULL) && (self->type == NRC_N_HOST_TYPE)) {
         switch (self->state) {
         case NRC_N_HOST_S_INITIALISED:
+            self->cfg = nrc_cfg_create(NULL, 0);
 
-            // Register callbacks
-            result = NRC_R_OK;
- 
-            if(OK(result)) {
+            // Register self as listener to all status messages
+            if (self->cfg != NULL) {
+                result = nrc_status_start_listen(NULL, self);
+            }
+
+            if (OK(result)) {
                 self->state = NRC_N_HOST_S_STARTED;
             }
             else {
@@ -210,7 +239,10 @@ static s32_t nrc_node_host_stop(nrc_node_t slf)
     if ((self != NULL) && (self->type == NRC_N_HOST_TYPE)) {
         switch (self->state) {
         case NRC_N_HOST_S_STARTED:
-            // Unregister callbacks
+            //nrc_cfg_destroy(self->cfg); TODO
+            self->cfg = NULL;
+
+            result = nrc_status_stop_listen(self);
 
             self->state = NRC_N_HOST_S_INITIALISED;
             result = NRC_R_OK;
@@ -225,16 +257,105 @@ static s32_t nrc_node_host_stop(nrc_node_t slf)
     return result;
 }
 
+static enum nrc_node_host_cmd get_cmd(jsmntok_t *tokens, s32_t num_tokens, s8_t *json)
+{
+    enum nrc_node_host_cmd cmd = NRC_N_HOST_CMD_INVALID;
+
+    if ((num_tokens > 2) && 
+        (tokens[1].type == JSMN_STRING) && (tokens[2].type == JSMN_STRING)) {
+
+        s8_t *str = &json[tokens[1].start];
+
+        if (strncmp(str, "cmd", 3) == 0) {
+            str = &json[tokens[2].start];
+
+            if (strncmp(str, "start", 5) == 0) {
+                cmd = NRC_N_HOST_CMD_START;
+            }
+            else if (strncmp(str, "stop", 4) == 0) {
+                cmd = NRC_N_HOST_CMD_STOP;
+            }
+        }
+
+    }
+
+    return cmd;
+}
+
+static s32_t cmd_start(struct nrc_node_host *self)
+{
+    s32_t result = NRC_R_OK;
+
+    result = nrc_os_start(self->cfg);
+
+    return result;
+}
+
+static s32_t node_cfg(struct nrc_node_host *self, const s8_t *n_cfg)
+{
+    s32_t       result = NRC_R_OK;
+    const s8_t  *cfg_node_id = NULL;
+
+    result = nrc_cfg_add_node(self->cfg, n_cfg, (u32_t)strlen(n_cfg), &cfg_node_id);
+
+    return result;
+}
+
+static s32_t handle_json(struct nrc_node_host *self, struct nrc_msg_str *msg)
+{
+    s32_t result = NRC_R_OK;
+
+    self->num_tokens = jsmn_parse(&self->json_parser, msg->str, strlen(msg->str), self->json_tokens, NRC_N_HOST_MAX_TOKENS);
+
+    if (self->num_tokens > 2) {
+        // Command
+        enum nrc_node_host_cmd cmd = get_cmd(self->json_tokens, self->num_tokens, msg->str);
+
+        switch (cmd) {
+        case NRC_N_HOST_CMD_START:
+            result = cmd_start(self);
+            break;
+        case NRC_N_HOST_CMD_STOP:
+            NRC_ASSERT(FALSE); // Not implemented
+            break;
+        default:
+            // Assume node configuration
+            result = node_cfg(self, msg->str);
+            break;
+        }
+    }
+
+    return result;
+}
+
+static s32_t handle_status(struct nrc_node_host *self, struct nrc_msg_status *msg)
+{
+    s32_t result = NRC_R_OK;
+
+    return result;
+}
+
 static s32_t nrc_node_host_recv_msg(nrc_node_t slf, nrc_msg_t msg)
 {
-    struct nrc_node_host  *self = (struct nrc_node_host*)slf;
+    struct nrc_node_host    *self = (struct nrc_node_host*)slf;
+    struct nrc_msg_hdr      *msg_hdr = (struct nrc_msg_hdr*)msg;
     s32_t                   result = NRC_R_INVALID_IN_PARAM;
 
     if ((self != NULL) && (self->type == NRC_N_HOST_TYPE) && (msg != NULL)) {
 
         switch (self->state) {
         case NRC_N_HOST_S_STARTED:
-            NRC_LOGW(_tag, "recv_msg(%s): unexpected msg %d", self->hdr.cfg_id);
+            if (msg_hdr->type == NRC_MSG_TYPE_STRING) {
+                // JSON commands or nodes
+                result = handle_json(self, (struct nrc_msg_str*)msg);
+            }
+            else if (msg_hdr->type == NRC_MSG_TYPE_STATUS) {
+                // Status messages
+                result = handle_status(self, (struct nrc_msg_status*)msg);
+            }
+            else {
+                NRC_LOGW(_tag, "recv_msg(%s): unexpected msg %d", self->hdr.cfg_id);
+            }
             break;
 
         default:
